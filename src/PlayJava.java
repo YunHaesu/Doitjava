646cb0e53f4a5167ebfcb758ab3b964fabd82fa9
YunHaesu <YunHaesu.dev@gmail.com>
2020-08-12 15:17:16 +0900

    JAVA

---
+++ src/PlayJava.java
@@ -0,0 +1,9 @@
+
+public class PlayJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ src/chapter02/BinaryTest.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+public class BinaryTest {
+
+	public static void main(String[] args) {
+		int bNum = 0B1010;	//2진수 0B를 붙이면
+		int dNum = 10;		//10진수
+		int oNum = 012;		//8진수 숫자앞에 0을 붙이면
+		int hNum = 0xA;		//16진수 0x를 붙이면
+
+		System.out.println(bNum);
+		System.out.println(dNum);
+		System.out.println(oNum);
+		System.out.println(hNum);
+
+	}
+
+}
