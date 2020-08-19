368c964231911d783952c30ded0a0f935208d68e
YunHaesu <YunHaesu.dev@gmail.com>
2020-08-19 13:44:18 +0900

    JAVA

---
+++ 2020-08-11/src/Info.java
@@ -0,0 +1,26 @@
+
+public class Info {
+	private String name;
+	private int age;
+	private String like;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getLike() {
+		return like;
+	}
+	public void setLike(String like) {
+		this.like = like;
+	}
+
+}
