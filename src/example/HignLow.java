3757b9337a00286973e64c644cf3402094202191
YunHaesu <YunHaesu.dev@gmail.com>
2020-09-01 13:49:23 +0900

    JAVA

---
+++ src/example/HignLow.java
@@ -0,0 +1,32 @@
+package example;
+
+import java.util.Scanner;
+
+public class HignLow {
+
+	public static void main(String[] args) {
+		int num = (int) (Math.random() * 100) + 1;
+		int cnt = 0;
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.println("정수를 입력해주세요");
+			System.out.print("> ");
+			int num2 = sc.nextInt();
+			if (num2 > num) {
+				System.out.println("down");
+				cnt++;
+				System.out.println(cnt + "회 입력");
+			} else if (num2 < num) {
+				System.out.println("up");
+				cnt++;
+				System.out.println(cnt + "회 입력");
+			} else {
+				System.out.println("정답입니다.");
+				System.out.println("정답은 " + num + "이며" + cnt + "회 입력하셨습니다.");
+				break;
+			}
+
+		}
+	}
+
+}
