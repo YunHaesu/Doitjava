b7a927444d8702383c44737c4d7641fc226a205b
YunHaesu <YunHaesu.dev@gmail.com>
2020-09-10 15:45:28 +0900

    JAVA

--- src/chapter05/Student.java
+++ src/chapter05/Student.java
@@ -6,7 +6,26 @@ public class Student {
 	int grade;
 	String address;
 
-	public void showStudentInfo() {		// 뒤에 소괄호가 있으면 메서드 없으면 클래스
+	/* 생성자 - 객체를 생성할 때 실행하는 블록 */
+	Student(int num) { // 매개변수가 없는 생성자
+		System.out.println("Student 객체를 생성합니다." + num);
+	} // 디폴트 생성자는 매개변수가 없고 실행 코드가 없는 생성자
+
+//	Student() {}	//디폴트 생성자 : 생성자를 명시하지 않으면 자동으로 생성
+	
+	Student() { // 매개변수가 없는 생성자
+		System.out.println("Student 객체를 생성합니다.");
+	}
+	Student(long num) { // 매개변수가 없는 생성자
+		System.out.println("Student 객체를 생성합니다." + num);
+	}
+	Student(int num,long num2) {
+		System.out.println("Student 객체를 생성합니다." + num);
+	}
+	Student(int num2,int num3) {
+		System.out.println("Student 객체를 생성합니다." );
+	}
+	public void showStudentInfo() { // 뒤에 소괄호가 있으면 메서드 없으면 클래스
 		System.out.println(studentName + ", " + address);
 	}
 }
